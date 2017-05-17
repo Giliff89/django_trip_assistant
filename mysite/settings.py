c8fcf4fc15528f31787f4ffb8387dc714b32de87
gina iliff <Giliff@me.com>
2017-05-17 15:33:38 -0700

    testing model with first user

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'trip_assistant.apps.TripAssistantConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- trip_assistant/models.py
+++ trip_assistant/models.py
@@ -2,38 +2,54 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Traveler(models.Model):
     """The user of Trip Assistant website."""
 
-    username = models.CharField(max_length=24)
+    username = models.CharField(max_length=24, unique=True)
     password = models.CharField(max_length=36)
-    email = models.CharField(max_length=64)
+    email = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.username
 
 
+@python_2_unicode_compatible
 class Trip(models.Model):
     """Info for a trip that belongs to a specific Traveler."""
 
-    traveler = models.ForeignKey(Traveler)
-    depart_date = models.DateTimeField('departure date')
-    return_date = models.DateTimeField('return date')
+    traveler = models.ForeignKey(Traveler, on_delete=models.CASCADE)
+    depart_date = models.DateField('departure date')
+    return_date = models.DateField('return date')
     days = models.IntegerField(default=1)
     destination = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.destination
 
+
+@python_2_unicode_compatible
 class Attraction(models.Model):
     """Business ids from Yelp API."""
 
     business_id = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.business_id
 
+
+@python_2_unicode_compatible
 class Recommendation(models.Model):
     """Record of attraction that has been recommended to a Traveler."""
 
     trip = models.ForeignKey(Trip)
-    attraction = models.ForeignKey(Attraction)
-    rec_value = models.IntegerField(default=0)  # value records if Traveler has voted rec up, down, or passed
+    attraction = models.ForeignKey(Attraction, on_delete=models.CASCADE)
+    rec_value = models.IntegerField(null=True)  # value records if Traveler has voted rec up, down, or passed
 
+    def __str__(self):
+        return self.attraction
 
-# Later, will add in classes to record a set itinerary for each day
+# Will add in classes to record a set itinerary for each day
